6de914861ebb5e713d45844227013417f9e04a6b
Metwally Mohamed <mtwliii999@gmail.com>
2026-04-07 11:14:57 +0200

    3

--- src/app/_providers/MySessionProvidr.tsx
+++ src/app/_providers/MySessionProvidr.tsx
@@ -1,14 +1,10 @@
 "use client"
 import { SessionProvider } from 'next-auth/react'
 
-export default function MySessionProvidr({ children }) {
+export default function MySessionProvidr({ children }: { children: React.ReactNode }) {
     return (
-        <>
-            <SessionProvider>
-
-                {children }
-                
-            </SessionProvider>
-        </>
+        <SessionProvider>
+            {children}
+        </SessionProvider>
     )
 }
